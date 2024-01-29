97f90d66b260a0f670b9cb27235b991292f48276
Владимир Андреевич <lantsovva@gmail.com>
2024-01-29 12:00:47 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config({path: '.env'});
+const {SerialPort} = require('serialport');
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({port: process.env.WEBSOCKET_PORT});
+let websocketClient;
+const portName = process.env.SERIAL_PORT;
+const baudRate = parseInt(process.env.BAUD_RATE);
+let port;
+
+wss.on('connection', function connection(ws) {
+    console.log('WS Connected.');
+    websocketClient = ws;
+});
+
+const connectToSerialPort = () => {
+    port = new SerialPort({path: portName, baudRate: baudRate}, function (err) {
+        if (err) {
+            console.error('Error when opening COM port:', err.message);
+            setTimeout(connectToSerialPort, 5000);
+        }
+    });
+
+    port.on('open', () => {
+        console.log('COM port is open.');
+    });
+
+    port.on('data', function (data) {
+        const buffer = Buffer.from(data);
+        const text = buffer.toString('ascii');
+        console.log('Received data:', text);
+        if (websocketClient) {
+            websocketClient.send(text);
+        }
+    });
+
+    port.on('close', () => {
+        console.log('The COM port was closed.');
+        setTimeout(connectToSerialPort, 5000);
+    });
+
+    port.on('error', (err) => {
+        console.error('COM port error:', err);
+        setTimeout(connectToSerialPort, 5000);
+    });
+};
+
+connectToSerialPort();
